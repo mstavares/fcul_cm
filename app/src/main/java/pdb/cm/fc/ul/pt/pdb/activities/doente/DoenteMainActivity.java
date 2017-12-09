01c7664b07a355f19b07c41b8d6ee4a6bb4583ce
Nuno Nelas <nunonelas@Mac-mini-de-Nuno.local>
2017-12-09 21:07:47 +0000

    adicionei logout

--- app/src/main/java/pdb/cm/fc/ul/pt/pdb/activities/doente/DoenteMainActivity.java
+++ app/src/main/java/pdb/cm/fc/ul/pt/pdb/activities/doente/DoenteMainActivity.java
@@ -5,20 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import pdb.cm.fc.ul.pt.pdb.R;
+import pdb.cm.fc.ul.pt.pdb.activities.LoginActivity;
 
 public class DoenteMainActivity extends AppCompatActivity {
 
     private static final String EXTRA_EMAIL = "email";
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doente_main);
         ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @OnClick(R.id.button1)
@@ -30,6 +36,13 @@ public class DoenteMainActivity extends AppCompatActivity {
     @OnClick(R.id.button2)
     public void button2() {
         startActivity(new Intent(this, EsferaActivity.class).putExtra("email", loadEmailFromExtras()));
+        finish();
+    }
+
+    @OnClick(R.id.button3)
+    public void button3() {
+        mAuth.signOut();
+        startActivity(new Intent(this, LoginActivity.class));
         finish();
     }
 
